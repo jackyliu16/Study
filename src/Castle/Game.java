30d73a1ed28bc2b549e8fb7971c4be35900c1a84
刘逸珑 <18922251299@163.com>
2021-11-06 15:14:53 +0800

    最终更改成为完整的框架 + 数据格式

--- src/Castle/Game.java
+++ src/Castle/Game.java
@@ -18,15 +18,30 @@ package castle;
         Room的方向是通过成员变量进行表示的，增加或者减少方向都需要对于代码进行改动
         但如果通过Hash表来表示方向，那么方向就不是硬编码的了【方向与Room没有关系了】
         【硬编码是将数据直接嵌入到程序或其他可执行对象的源代码中的软件开发实践，与从外部获取数据或在运行时生成数据不同。】
-
+    4.  以框架 + 数据来提高代码 的可扩展性
+        对于Room进行改造，使原来【通过成员变量进行硬编码】，变成用容器。
+        在Room中我们就创建了一个框架，也就是cmd和房间之间的对应关系
+        思路：
+        把程序尽可能的解成框架 + 数据的格式
+        【在本次更新中主要通过Handler类来处理命令】
+        用hash表来保存命令和Handler之间的关系【使命令解析脱离if-else的结构】
+            [ 由于hash表要求的是key - value(二者都是对象),但是想要达到key-function]
+            因此要通过类间接调用函数
+        注意：相对于给出的代码，此处在Handler函数中还额外的对于Game函数进行了重写
+        【不知道为什么之前会报栈溢出的错误，但是现在似乎莫名其妙的好了？】
  */
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Game {
     private Room currentRoom;
+    private HashMap< String, Handler> handlers = new HashMap<String,Handler>();
         
     public Game() 
     {
+        handlers.put("go",new HandlerGo(this));
+        handlers.put("bye",new HandlerBye(this));
+        handlers.put("help",new HandlerHelp(this));
         createRooms();
     }
 
@@ -71,18 +86,33 @@ public class Game {
         System.out.println(currentRoom.getExitDesc());
         System.out.println();
     }
+
+    public void play(){
+        Scanner in = new Scanner(System.in);
+        while ( true ) {
+            String line = in.nextLine();
+            String[] words = line.split(" ");
+            Handler handler = handlers.get(words[0]);
+            String value = "";
+            if ( words.length > 1 )
+                value = words[1];
+            if ( handler != null ){
+                handler.doCmd(value);
+                // 这个地方我感觉不是很正确，应当采用先后顺序法，
+                // 先判断第一个是否bye然后再说别的，传输一个空的参数感觉还是有点危险
+                if( handler.isBye() ){
+                    break;
+                }
+            }
+
+        }
+        in.close();
+    }
     // 以下为用户命令
 
-    private void printHelp() 
-    {
-        System.out.print("迷路了吗？你可以做的命令有：go bye help");
-        System.out.println("如：\tgo east");
-    }
-
-    private void goRoom(String direction) 
+    public void goRoom(String direction)
     {
         Room nextRoom = currentRoom.getExit(direction);
-
         if (nextRoom == null) {
             System.out.println("那里没有门！");
         }
@@ -93,24 +123,14 @@ public class Game {
     }
 	
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+
 		Game game = new Game();
 		game.printWelcome();
+        game.play();
 
-        while ( true ) {
-        		String line = in.nextLine();
-        		String[] words = line.split(" ");
-        		if ( words[0].equals("help") ) {
-        			game.printHelp();
-        		} else if (words[0].equals("go") ) {
-        			game.goRoom(words[1]);
-        		} else if ( words[0].equals("bye") ) {
-        			break;
-        		}
-        }
         
-        System.out.println("感谢您的光临。再见！");
-        in.close();
+        System.out.println("感谢您的测试，再见！");
+
 	}
 
 }
---
+++ src/Castle/Handler.java
@@ -0,0 +1,32 @@
+package castle;
+
+public class Handler{
+    protected Game game;
+
+    public Handler(Game game){
+        this.game = game;
+    }
+
+    public void doCmd(String word) {
+
+    }
+
+    public boolean isBye(){
+        return false;
+    }
+
+//    public void Game(){
+//
+//    }
+
+//    @Override
+//    public void showPrompt() {    }
+//
+//    @Override
+//    public void play() {    }
+//
+//    public Handler() {
+//
+//    }
+}
+
