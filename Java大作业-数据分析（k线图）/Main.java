4855385335297400a863ccd23c3f28cef88372ad
刘逸珑 <18922251299@163.com>
2021-11-10 15:02:39 +0800

    Data修正前上传

--- Java大作业-数据分析（k线图）/Main.java
+++ Java大作业-数据分析（k线图）/Main.java
@@ -91,27 +91,18 @@ public class Main {
      */
     static Double[][] analyse(String[][] a, String[] split) {
         Double[][] recode = new Double[split.length - 1][4];
-        // recode = {top,bottom,max,min}
-        // recode =  0   1    2     3
-        // a      = {Date,Open,High,Low,Close,Adj Close,Volume}
-        // a      =   0    1     2   3    4      5        6
-        int sp = 0;     // 指向
-        // 对于第一项进行初始化操作
+        /*
+         recode = {top,bottom,max,min}
+                     0   1    2     3
+         a      = {Date,Open,High,Low,Close,Adj Close,Volume}
+                     0    1     2   3    4      5        6
+        */
+        int sp = 0;     // the index of year range
+        // recode the start of the first year
         recode[0][0] = Double.parseDouble(a[1][1]);
-//        recode[0][1] = Double.parseDouble(a[1][4]);
         for( int i = 1; i < a.length ; i++){
-            /*System.out.println("---------------Start----------------");
-            System.out.println("i: " + i);
-            for (Double[] strings : recode) {
-                for (int j = 0; j < recode[0].length; j++) {
-                    System.out.print(strings[j]);
-                    System.out.print("|");
-                }
-                System.out.println();
-            }
-            System.out.println("-------------end-------------------");*/
-            // 从头到尾对所有行进行计算
-//            if (a[i].contains())
+            // if recode is null -> add someone to it
+            // if it wasn't null -> using bubble sort to find biggest and smallest one in a year
             if (sp != 0) {
                 if (recode[sp][2] == null)
                     recode[sp][2] = Double.parseDouble(a[i-1][2]);
@@ -125,41 +116,23 @@ public class Main {
                     recode[sp][3] = Double.parseDouble(a[i][3]);
             }
             if (aInSection(a[i][0],split,sp)){
-                // 如果 i 位于[sp,sp+1]区间中
+                // if a in range(sp,sp+1]year. -> using bubble sort to find biggest and smallest one.
                 recode[sp][2] = findBig(recode[sp][2],a[i][2]);
                 recode[sp][3] = findSmall(recode[sp][3],a[i][3]);
 /*
             StdOut.println("a[i]:" + Arrays.toString(a[i]));
-            System.out.println("***************Start----------------");
-            System.out.println("i: " + i);
-            for (Double[] strings : recode) {
-                for (int j = 0; j < recode[0].length; j++) {
-                    System.out.print(strings[j]);
-                    System.out.print("|");
-                }
-                System.out.println();
-            }
-            System.out.println("-------------end-------------------");
 */
             } else {
-                // 如果 一旦不能满足要求，则视为已经达到区分位置，此时如果没有达到split的尾，则进行指针移动操作
+                // if cross the border
 
                 if (sp < split.length -1) {
                     if (sp != split.length - 2)
                         recode[sp + 1][0] = Double.parseDouble(a[i][1]);
                     recode[sp][1] = Double.parseDouble(a[i-1][4]);
-//
-//                    if (recode[sp][2] == null)
-//                        recode[sp][2] = Double.parseDouble(a[i - 1][2]);
-//                    if (recode[sp][3] == null)
-//                        recode[sp][3] = Double.parseDouble(a[i - 1][3]);
                     sp++;
-//                    System.out.println("SP :" +sp);
-//                    System.out.println("---------------------------------------------");
-
                 }
                 else if (sp == split.length - 1){
-                    //进行最后一次操作，并且输出结果
+                    // if it's the last year
                     recode[sp][1] = Double.parseDouble(a[i][4]);
 
                     return recode;
@@ -208,21 +181,13 @@ public class Main {
             StdDraw.filledRectangle(x,(a[i][0] + a[i][1]) / 2.0 , wSquare * 0.5, Math.abs(a[i][0] - a[i][1]) / 2.0);
             StdDraw.line(x,a[i][2],x,a[i][3]);
         }
-        //pen radius = 1/4 * width
-
-        /*for ( int i = 0; i < a.length; i++){
-            double x = (1/2) * sp + WIDTH_Total * i;
-            StdDraw.setPenRadius((1.0/2) * wSquare);
-            StdDraw.line(x,a[i][0],x,a[i][1]);
-        }*/
-
-
+        StdDraw.save( "output.png");
     }
 
 
     public static void main(String[] args) throws IOException {
         Scanner in = new Scanner(System.in);
-        System.out.println("在开始之前请确认本文件被放置于src目录中，并且src目录中包含data目录[里面放有需要进行绘图的数据]");
+        System.out.println("please makesure this file is been put at a folder which is name src, with a subfolder name data , which incluing the data you want");
         System.out.println("中文注释： 此处请输入一个在src的子目录data中的csv文件名称(注：包含.csv)");
         System.out.println("please give a filename under a file(data) at the same catalogue: ");
         String filename = in.next();
@@ -230,10 +195,6 @@ public class Main {
         // import file to Stream
         Path path = Paths.get("src/data/" + filename);
         List<String> contents = Files.readAllLines(path, Charset.defaultCharset());
-//            object[] apple = contents.toArray();
-//            for (String b: contents){
-//                System.out.println(b);
-//            }
 
         // get the number who not contain null
         int line = 0; //account the number of line
@@ -261,15 +222,6 @@ public class Main {
             D2[i] = D1[i].split(",");
         }
 
-        // print out everything(2D) from data (String[][])
-        /*for (String[] strings : D2) {
-            for (int j = 0; j < D2[0].length; j++) {
-                System.out.print(strings[j]);
-                System.out.print("|");
-            }
-            System.out.println();
-        }*/
-
         /* Create split_Year Array for aInSection to Separate the data ( from first col)  */
         String maxtime = D2[line - 1][0];
         String mintime = D2[1][0];
@@ -279,14 +231,13 @@ public class Main {
         // create a array of the first year to last year
         int startyear = Integer.parseInt(mintime.split("-")[0]);
         int[] split_time = new int[YearAcross + 1];
-        for ( int i = 0; i <= YearAcross; i++){
-//                StdOut.println(acc + i);
+
+        for ( int i = 0; i <= YearAcross; i++)
             split_time[i] = startyear + i;
-        }
 
         String[] split_year = new String[YearAcross+1];
         // BC aInSection using (sp,sp+1] so we need to move the first year forward by one
-        {
+        {   // we use blank is because we don't want to create more Variable.
             String[] apple = mintime.split("-");        // getting the first time of Data
             int banana = Integer.parseInt(apple[2]);
             banana--;                                         // advance the date by one
@@ -298,11 +249,6 @@ public class Main {
             split_year[i] = split_time[i] + "-10-15" ;
         }       // add mon and day to data
 
-        // print the split of year
-        /*or (int i = 0; i < split_time.length ; i++){
-            System.out.println(split_time[i]);
-        }*/
-
         Double[][] output = analyse(D2,split_year);
         double[][] OutPut = new double[output.length][output[0].length];
         for ( int i = 0 ; i < output.length; i++){
@@ -311,13 +257,13 @@ public class Main {
             }
         }
         // print out the OutPut Array
-        /*for(double[] strings : OutPut){
+        for(double[] strings : OutPut){
             for ( int j = 0; j < OutPut[0].length ; j++){
                 System.out.print(strings[j]);
                 System.out.print("|");
             }
             System.out.println();
-        }*/
+        }
         Draw(OutPut);
     }
 
