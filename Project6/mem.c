a9ea42915fae9705a423330c3dfa69ec481c25bd
jackyLiu <18922251299@163.com>
2022-05-11 01:42:21 +0800

    finish debug

--- Project6/mem.c
+++ Project6/mem.c
@@ -30,8 +30,133 @@ MemoryBlock* init(){
     return block;
 }
 
+// split a node size into two nodes and the size of want_value is the node split_node's value
+void split_node(MemoryBlock *pList, Node *split_node, int want_value, Node *result_fragment);
+//[FF] from head sequential search to find the size which could matching the need
+void ask_for_memory(MemoryBlock *block, int value);
+// print out the allocated memory
+void print_memory_allocation(const MemoryBlock *pBlock);
+// release memory from designated location
+void release_memory(MemoryBlock *block, Node *release );
+// asking for release memory from specified index
+void ask_for_release(MemoryBlock *block, int pid);
+// get the index of MemoryBlock
+int length_account(MemoryBlock *block);
+
+int main(int *argc, char **argv){
+    MemoryBlock *block = init();
+    ask_for_memory(block, 20);
+    ask_for_memory(block, 5);
+    ask_for_memory(block, 23);
+
+    print_memory_allocation(block);
+
+    // release_memory(block, block->head);
+    ask_for_release(block, 0);
+
+    print_memory_allocation(block);
+}
+
+void ask_for_release(MemoryBlock *block, int pid){
+    int length = length_account(block);
+    if ( pid >= length ) {
+        printf("pid not existed");
+        return;
+    }
+    int account = 0;
+    Node *p = block->head;
+    while ( p != NULL && account == pid-1 ){
+        account ++;
+        p=p->next;
+    }
+    if ( !p->hasBeenUsed ) {
+        printf("this memory peace hasn't been used yet\n");
+        return ;
+    }
+    release_memory(block, p);
+}
+
+void release_memory(MemoryBlock *block, Node *release ) {
+    release->hasBeenUsed = false;
+
+    Node *p;
+    // if prev is unused
+    if ( release->next && ! release->next->hasBeenUsed ) {
+        p = release->next;
+        release->size += release->next->size;
+        if ( release->next->next ) {
+            release->next = release->next->next;
+            release->next->prev = release;
+        }
+        else {
+            release->next = NULL;
+            block->tail = release;
+        }
+        free(p);
+    }
+
+    // if next is unused
+    if ( release->prev && !release->prev->hasBeenUsed ) {
+        release->size += release->next->size;
+        p = release->prev;
+        if ( release->prev->prev ) {
+            release->prev = release->prev->prev;
+            release->prev->next = release;
+        }
+        else {
+            release->prev = NULL;
+            block->head = release;
+        }
+        free(release);
+    }
+}
+
+int length_account(MemoryBlock *block) {
+    int account = 0;
+    Node *p = block->head;
+    while( p != NULL ) {
+        account ++;
+        p = p->next;
+    }
+    return account;
+}
+
+void print_memory_allocation(const MemoryBlock *pBlock) {
+    int account = 0 ;
+    printf("\nOUTPUT:\nsize:\t");
+    Node *p, *q;
+    for ( p = pBlock->head; p != NULL; p =p->next ) {
+        printf("%d\t", p->size);
+        account ++;
+    }
+    printf("\nused:\t");
+    for ( p = pBlock->head; p != NULL; p =p->next ) {
+        printf("%d\t", p->hasBeenUsed);
+    }
+    printf("\npid:\t");
+    for ( int i = 0 ; i < account ; i++ ) {
+        printf("%d\t", i);
+    }
+    printf("\n");
+}
+
+void ask_for_memory(MemoryBlock *block, int value) {
+    // from head to tail find a space which contails areas >= need and hasn't been used yet
+    Node *p = block->head, *q;
+    // delete the node and try to split it into two parts
+    // one parts is matching the need, and others parts are (total - value)
+    while ( p != NULL ) {
+        if ( p->size >= value ) {
+            split_node(block, p, value, q);
+            p->hasBeenUsed = true;
+            break;
+        }
+        p=p->next;
+    }
+}
+
 // split a node into two pieces
-void split_node(MemoryBlock *pList, Node *split_node, int want_value, Node *result_fragment){
+void split_node(MemoryBlock *pList, Node *split_node, int want_value, Node *result_fragment) {
     result_fragment = (Node *)malloc(sizeof(Node));
     result_fragment->hasBeenUsed  = false;
  
@@ -64,80 +189,4 @@ void split_node(MemoryBlock *pList, Node *split_node, int want_value, Node *resu
         
     // }
     printf("end");
-}
-
-void print_memory_allocation( MemoryBlock *pBlock) {
-    printf("\nOUTPUT:\n");
-    Node *p, *q;
-    for ( p = pBlock->head; p != NULL; p =p->next ) {
-        printf("%d\t", p->size);
-    }
-    printf("\n");
-    for ( p = pBlock->head; p != NULL; p =p->next ) {
-        printf("%d\t", p->hasBeenUsed);
-    }
-    printf("\n");
-
-}
-
-void ask_for_memory(MemoryBlock *block, int value) {
-    // from head to tail find a space which contails areas >= need and hasn't been used yet
-    Node *p = block->head, *q;
-    // delete the node and try to split it into two parts
-    // one parts is matching the need, and others parts are (total - value)
-    while ( p != NULL ) {
-        if ( p->size >= value ) {
-            split_node(block, p, value, q);
-            p->hasBeenUsed = true;
-            break;
-        }
-        p=p->next;
-    }
-}
-
-void release_memory(MemoryBlock *block, Node *release ) {
-    release->hasBeenUsed = false;
-
-    Node *p, *q, *s;
-    // if prev is unused
-    if ( release->next && ! release->next->hasBeenUsed ) {
-        release->size += release->next->size;
-        if ( release->next->next ) {
-            release->next = release->next->next;
-            release->next->prev = release;
-        }
-        else {
-            release->next = NULL;
-            block->tail = release;
-        }
-        free(release->next);
-    }
-
-    // if next is unused
-    if ( release->prev && !release->prev->hasBeenUsed ) {
-        release->size += release->next->size;
-
-        if ( release->prev->prev ) {
-            release->prev = release->prev->prev;
-            release->prev->next = release;
-        }
-        else {
-            release->prev = NULL;
-            block->head = release;
-        }
-    }
-}
-
-int main(){
-    MemoryBlock *block = init();
-    ask_for_memory(block, 20);
-    ask_for_memory(block, 5);
-    ask_for_memory(block, 23);
-
-    print_memory_allocation(block);
-
-    release_memory(block, block->head);
-
-    print_memory_allocation(block);
-
 }
