5cc1c42359a060592e412510b95447331f414148
yilongLiu <18922251299@163.com>
2022-09-20 02:04:06 +0800

    update finish 2.2
    	todo using *argc to cut down the function constant
    	modified:   HW2/main.py

--- HW2/main.py
+++ HW2/main.py
@@ -23,16 +23,27 @@ def even_change(image: np.ndarray, multiple: float) ->  np.ndarray:
     tmp_image[tmp_image > 255] = 255
     return tmp_image
 
-def range_even_change(image: np.ndarray, multiple: float, left: int, right: int) -> np.ndarray:
+# TODO using *args to cut down the number of arguments and using one function constant for zero and low value
+def range_even_change(image: np.ndarray, multiple: float, left: int, right: int, func) -> np.ndarray:
     tmp = np.zeros(image.shape)
     for dim in range(0, image.shape[2]):
         for i in range(image.shape[0]):
             for j in range(image.shape[1]):
                 if image[i][j][dim] >= left and image[i][j][dim] < right:
-                    tmp[i][j][dim] = multiple * image[i][j][dim]
+                    tmp[i][j][dim] = func(image[i][j][dim], multiple)
                 else:
                     tmp[i][j][dim] = image[i][j][dim]
     return tmp
+
+# implement of func
+def even(num: int, multiple: int) -> int:
+    return num * multiple
+
+def zero(num: int, multiple:int ) -> int:
+    return 0
+
+def constant(num: int, multiple: int) -> int:
+    return 50
 
 if __name__ == "__main__":
     plt = plt.imread("OIP-C.jpg")
@@ -50,12 +61,17 @@ if __name__ == "__main__":
     # show_original_image(darked_img)
 
     # Even brighter [100, 150)
-    # range_brighter = range_even_change(plt_gray, 1.1, 100, 150)
-    # show_original_image(range_brighter)
+    range_brighter = range_even_change(plt_gray, 1.1, 100, 150, even)
+    show_original_image(range_brighter)
 
-
-
-
+    # for [0, 80) and [150, 255) low grey value
+    tmp_a = range_even_change(plt_gray, 0, 0, 80, constant)
+    low_grey = range_even_change(tmp_a, 0, 150, 255, constant)
+    show_original_image(low_grey)
+    # 0 value
+    tmp_b = range_even_change(plt_gray, 0, 0, 80, zero)
+    zero_graph = range_even_change(tmp_a, 0, 150, 255, zero)
+    show_original_image(zero_graph)
 
     print("end")
 
